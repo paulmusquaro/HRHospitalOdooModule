fe74ea354e68487ff72e86eb42c10112fb062744
paulmusquaro <paulren9200@ukr.net>
2025-08-04 14:17:41 +0300

    expand the kanban view for doctors

--- models/doctor.py
+++ models/doctor.py
@@ -13,11 +13,17 @@ class Doctor(models.Model):
     mentor_id = fields.Many2one('hr.hospital.doctor', string="Mentor", domain="[('is_intern','=',False)]")
     intern_ids = fields.One2many('hr.hospital.doctor', 'mentor_id', string="Interns")
     visit_ids = fields.One2many('hr.hospital.visit', 'doctor_id', string="Visits")
+    interns_names = fields.Char(string="Іменa інтернів", compute='_compute_interns_names', store=True)
 
     @api.depends('first_name', 'last_name')
     def _compute_name(self):
         for rec in self:
             rec.name = f"{rec.first_name or ''} {rec.last_name or ''}".strip()
+
+    @api.depends('intern_ids.name')
+    def _compute_interns_names(self):
+        for rec in self:
+            rec.interns_names = ', '.join(rec.intern_ids.mapped('name') or [])
 
     def fields_view_get(self, view_id=None, view_type='form', toolbar=False, submenu=False):
         res = super().fields_view_get(view_id, view_type, toolbar, submenu)
